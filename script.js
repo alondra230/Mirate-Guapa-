61878367d307d8b9f87b777aa226af571edf262f
alondra230 <rodasalondra10@gmail.com>
2026-08-12 22:47:36 -0600

    Crear Carrito y Calcular El total

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 let carrito = [];
 
 const botones = document.querySelectorAll(".producto button");
+const listaCarrito = document.getElementById("lista-carrito");
+const totalElemento = document.getElementById("total");
+const botonVaciar = document.getElementById("vaciar-carrito");
 
 botones.forEach((boton) => {
     boton.addEventListener("click", () => {
 
         const producto = boton.parentElement;
         const nombre = producto.querySelector("h3").textContent;
-        const precio = producto.querySelector("p").textContent;
+        const precioTexto = producto.querySelector("p").textContent;
+        const precio = Number(precioTexto.replace("Q", ""));
 
         carrito.push({
             nombre: nombre,
             precio: precio
         });
 
-        alert(nombre + " agregado al carrito");
+        actualizarCarrito();
 
-        console.log("Carrito:", carrito);
+        document.getElementById("carrito").scrollIntoView({
+            behavior: "smooth"
+        });
     });
+});
+
+function actualizarCarrito() {
+
+    listaCarrito.innerHTML = "";
+
+    if (carrito.length === 0) {
+        listaCarrito.innerHTML = "<p>Tu carrito está vacío.</p>";
+        totalElemento.textContent = "Total: Q0";
+        return;
+    }
+
+    let total = 0;
+
+    carrito.forEach((producto, indice) => {
+
+        total += producto.precio;
+
+        const elemento = document.createElement("div");
+
+        elemento.innerHTML = `
+            <p>
+                <strong>${producto.nombre}</strong> - Q${producto.precio}
+                <button onclick="eliminarProducto(${indice})">
+                    Eliminar
+                </button>
+            </p>
+        `;
+
+        listaCarrito.appendChild(elemento);
+    });
+
+    totalElemento.textContent = "Total: Q" + total;
+}
+
+function eliminarProducto(indice) {
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+botonVaciar.addEventListener("click", () => {
+    carrito = [];
+    actualizarCarrito();
 });
