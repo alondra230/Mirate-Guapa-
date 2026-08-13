e81523edd12b603be6fb4482332b2378b215a8bd
alondra230 <rodasalondra10@gmail.com>
2026-08-12 22:43:31 -0600

    activar carrito de compras

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
+let carrito = [];
 
-    const boton = document.querySelector("button");
+const botones = document.querySelectorAll(".producto button");
 
-    boton.addEventListener("click", function () {
-        document.getElementById("productos").scrollIntoView({
-            behavior: "smooth"
+botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+
+        const producto = boton.parentElement;
+        const nombre = producto.querySelector("h3").textContent;
+        const precio = producto.querySelector("p").textContent;
+
+        carrito.push({
+            nombre: nombre,
+            precio: precio
         });
-    });
 
+        alert(nombre + " agregado al carrito");
+
+        console.log("Carrito:", carrito);
+    });
 });
